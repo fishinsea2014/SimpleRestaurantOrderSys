7d88bda513bdc1e92134c23e0749aed007a71799
Jason Qu <fishinsea2014@gmail.com>
2018-11-01 01:32:14 +1300

    Setting up the project. Doing the SQLitehelper lib.

---
+++ RestaurantOrdering/CaterDal/SqliteHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+
+namespace CaterDal
+{
+    public static class SqliteHelper
+    {
+        //Read the connection string from configuration file.
+        private static string connStr = ConfigurationManager.ConnectionStrings["itcastCater"].ConnectionString;
+
+        public static int ExecuteNonQuery(string sql, params SQLiteParameter[] ps)
+        {
+            //Creating a connection object.
+            using (SQLiteConnection conn=new SQLiteConnection(connStr))
+            {
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddRange(ps);
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+
+            
+        }
+
+    }
+}
